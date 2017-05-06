fa0b15210deb794b141ecb4cf81539e34f3a89f7
Bob Deckard <bdeckard92@gmail.com>
2017-05-06 07:31:32 -0500

    heroku

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,7 @@
 // ===============================================================================
 var friends = require("../data/friends.js");
 
-var tableData = require("../data/surveyData");
+var surveyData = require("../data/surveyData");
 
 
 
@@ -15,81 +15,81 @@ var tableData = require("../data/surveyData");
 
 module.exports = function(app) {
 
-  app.get("/api/friends", function(req, res){
-    res.json(friends);
-  });
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
 
-  app.get("/api/friends". function(req, res){
-    var bestMatch = {
-      name: "",
-      photo: "",
-      friendDifference: 1000
-    };
-    console.log(req.body);
-    var userData = req.body;
-    var userScores = userData.scores;
+    app.post("/api/friends",function(req, res) {
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+        console.log(req.body);
+        var userData = req.body;
+        var userScores = userData.scores;
 
-    console.log(userScores);
+        console.log(userScores);
 
-    var totalDifference = 0;
+        var totalDifference = 0;
 
-    for (var i = 0; i < friends.length; i ++){
-      console.log(friends[i]);
-      totalDifference = 0;
+        for (var i = 0; i < friends.length; i++) {
+            console.log(friends[i]);
+            totalDifference = 0;
 
-      for (var j= 0; j < friends[i].scores[j]; j ++) {
-        totalDifference += Math.abs(parseInt(userScores[j]) = parseInt(friends[i].scores[j]))
+            for (var j = 0; j < friends[i].scores[j]; j++) {
+                totalDifference += Math.abs(parseInt(userScores[j]) = parseInt(friends[i].scores[j]))
 
-        if (totalDifference == bestMatch.friendDifference) {
-          bestMatch.name = friends[i].name;
-          bestMatch.photo = friends[i].photo;
-          bestMatch.friendDifference = totalDifference;
+                if (totalDifference <= bestMatch.friendDifference) {
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.friendDifference = totalDifference;
+                }
+            }
         }
-      }
-    }
 
-    friends.push(userData);
-    res.json(bestMatch);
-  });
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+        friends.push(userData);
+        res.json(bestMatch);
+    });
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
 
-  app.get("/api/survey", function(req, res) {
-    res.json(tableData);
-  });
+    app.get("/api/survey", function(req, res) {
+        res.json(surveyData);
+    });
 
-  
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate Javascript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
 
-  app.post("/api/survey", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    if (surveyData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-   
-  });
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate Javascript array
+    // (ex. User fills out a reservation request... this data is then sent to the server...
+    // Then the server saves the data to the tableData array)
+    // ---------------------------------------------------------------------------
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+    app.post("/api/survey", function(req, res) {
+        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+        // It will do this by sending out the value "true" have a table
+        if (surveyData.length < 5) {
+            tableData.push(req.body);
+            res.json(true);
+        }
 
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    tableData = [];
-    waitListData = [];
+    });
 
-    console.log(tableData);
-  });
+    // ---------------------------------------------------------------------------
+    // I added this below code so you could clear out the table while working with the functionality.
+    // Don"t worry about it!
+
+    /*app.post("/api/clear", function() {
+        // Empty out the arrays of data
+        tableData = [];
+        waitListData = [];
+
+        console.log(tableData);
+    });*/
 };
--- server.js
+++ server.js
@@ -9,9 +9,10 @@ var app = express();
 
 var PORT = 3000;
 
-app.use(bodyParser.json());
+
+app.use(bodyParser.json({type: "application/*+json"}));
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
+app.use(bodyParser.text({type: "text/html"}));
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 require("./app/routing/apiRoutes.js")(app);
